dda8cefe5262be30b33c5612eca2675a1e837830
Unknown <wtstas@gmail.com>
2019-05-15 15:06:04 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ src/test/java/UserTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+
+    @Test
+    void UserTest() {
+        Assertions.assertTrue(true);
+    }
+
+}
